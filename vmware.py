3617bc9e7b51c380403df645d7a10e4e17efbfa2
Lei Tu <alex.lei.tu@gmail.com>
2013-10-21 14:55:47 +0800

    add functions

--- vmware.py
+++ vmware.py
@@ -1,8 +1,8 @@
 from pysphere import VIServer
 import sys
 
-def main():
 
+def main():
     if len(sys.argv) <> 3:
         print ("Usage:")
         print (sys.argv[0], "<username> <password")
@@ -20,6 +20,82 @@ def main():
     version = server.get_api_version()
 
     print ("You are now connecting with {0} with API version {1}".format(type, version))
+
+
+    select(server)
+    server.disconnect()
+
+
+def shutdown_vm(server):
+    datacenter = 'HUBCNSH'
+    pathlist = server.get_registered_vms(datacenter = datacenter,advanced_filters={'runtime.powerState':['poweredOn', 'suspended']})
+    total = 0
+
+    for path in pathlist:
+        total = total + 1
+        print total, path.split('/')[1][:-4]
+
+    print ("=" * 30)
+    print ("Enter the number which server you would like to force shutdown")
+
+    number=raw_input(">")
+    i = int(number) - 1
+    vm = server.get_vm_by_path(pathlist[i])
+    vm.power_off()
+    print ("Job done!")
+
+def start_vm(server):
+    datacenter = 'HUBCNSH'
+    pathlist = server.get_registered_vms(datacenter = datacenter,advanced_filters={'runtime.powerState':['poweredOff']})
+    total = 0
+    print ("Here are the machines are off.")
+
+    for path in pathlist:
+        total = total + 1
+        print total, path.split('/')[1][:-4]
+
+    print("=" * 30)
+    print ("Enter the number which server you would like to bring it up")
+    number=raw_input(">")
+    i = int(number) - 1
+    vm = server.get_vm_by_path(pathlist[i])
+    vm.power_on()
+    print ("Job done!")
+
+def reset_vm(server):
+    datacenter = 'HUBCNSH'
+    pathlist = server.get_registered_vms(datacenter = datacenter,advanced_filters={'runtime.powerState':['poweredOn', 'suspended']})
+    total = 0
+
+    for path in pathlist:
+        total = total + 1
+        print total, path.split('/')[1][:-4]
+
+    print ("=" * 30)
+    print ("Enter the number which server you would like to force shutdown")
+
+    number=raw_input(">")
+    i = int(number) - 1
+    vm = server.get_vm_by_path(pathlist[i])
+    vm.reset()
+    print ("Job done!")
+
+def select(server):
+    #get_vm(server)
+    print ("what kind of methods you want to approach mode(start/shutdown/reset)")
+
+    answer = raw_input(">")
+    if answer == "start":
+        start_vm(server)
+    elif answer == "reset":
+        reset_vm(server)
+    elif answer == "shutdown":
+        shutdown_vm(server)
+    else:
+        print ("We don't know what your enter is")
+        sys.exit(1)
+
+
 
 
 if __name__ == "__main__":
